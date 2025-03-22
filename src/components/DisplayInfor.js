42243aff4135a531bfa59f33531075c858a3fa1f
Vinhz <bachthanhvinh12@gmail.com>
2025-03-22 21:37:33 +0700

    bai-34 stateFul/stateless component

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,85 +1,73 @@
 import React from "react";
 import "./DisplayInfor.scss";
 import logo from "./../logo.svg";
-class DisplayInfor extends React.Component {
-  constructor(props) {
-    console.log("call constructor: 1");
-    super(props);
-    this.state = {
-      isShowListUser: true,
-    };
-  }
+//stateless vs stateful
+// class DisplayInfor extends React.Component {
+//   render() {
+//     console.log(">> call me render : ");
+//     // distructuring
+//     const { listUsers, users } = this.props;
+//     return (
+//       <>
+//         <div className="display-infor-container">
+//           {true && (
+//             <div>
+//               {listUsers.map((user, index) => {
+//                 // console.log(">>>> check map user", user);
 
-  componentDidMount() {
-    console.log(">>> call me component did mount");
-    setTimeout(() => {
-      document.title = "học lập trình";
-    }, 3000);
-  }
+//                 return (
+//                   <div
+//                     key={user.id}
+//                     className={+user.age > 18 ? "green" : "red"}
+//                   >
+//                     <div>My name {user.name} </div>
+//                     <div>My age {user.age}</div>
+//                     <div>
+//                       <button
+//                         onClick={() => this.props.handleDeleteUser(user.id)}
+//                       >
+//                         X
+//                       </button>
+//                     </div>
+//                   </div>
+//                 );
+//               })}
+//             </div>
+//           )}
+//         </div>
+//       </>
+//     );
+//   }
+// }
 
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    console.log(">>> call me component did update", this.props, prevState);
+const DisplayInfor = (props) => {
+  const { listUsers, users } = props;
 
-    if (this.props.listUsers !== prevProps.listUsers) {
-      if (this.props.listUsers.length === 5) {
-        alert("You got 5 users");
-      }
-    }
-  }
-
-  handleShowHide = () => {
-    this.setState({
-      isShowListUser: !this.state.isShowListUser,
-    });
-  };
-
-  render() {
-    console.log(">> call me render : ");
-    // distructuring
-    const { listUsers, users } = this.props;
-    return (
-      <>
-        <div className="display-infor-container">
-          {/* <img src={logo} /> */}
+  return (
+    <>
+      <div className="display-infor-container">
+        {true && (
           <div>
-            <span
-              onClick={(e) => {
-                this.handleShowHide();
-              }}
-            >
-              {this.state.isShowListUser === true
-                ? "Hide list users: "
-                : "Show list users:"}
-            </span>
-          </div>
-          {this.state.isShowListUser && (
-            <div>
-              {listUsers.map((user, index) => {
-                // console.log(">>>> check map user", user);
+            {listUsers.map((user, index) => {
+              // console.log(">>>> check map user", user);
 
-                return (
-                  <div
-                    key={user.id}
-                    className={+user.age > 18 ? "green" : "red"}
-                  >
-                    <div>My name {user.name} </div>
-                    <div>My age {user.age}</div>
-                    <div>
-                      <button
-                        onClick={() => this.props.handleDeleteUser(user.id)}
-                      >
-                        X
-                      </button>
-                    </div>
+              return (
+                <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+                  <div>My name {user.name} </div>
+                  <div>My age {user.age}</div>
+                  <div>
+                    <button onClick={() => props.handleDeleteUser(user.id)}>
+                      X
+                    </button>
                   </div>
-                );
-              })}
-            </div>
-          )}
-        </div>
-      </>
-    );
-  }
-}
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
 
 export default DisplayInfor;
