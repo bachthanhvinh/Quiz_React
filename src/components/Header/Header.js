3650b5436cbf4d2108e2f381fc642f84a134e6b0
Vinhz <bachthanhvinh12@gmail.com>
2025-03-24 22:03:41 +0700

    #50 active link - navlink

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -12,15 +12,15 @@ const Header = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Link to="/" className="nav-link">
+            <NavLink to="/" className="nav-link">
               Home
-            </Link>
-            <Link className="nav-link" to="Users">
+            </NavLink>
+            <NavLink className="nav-link" to="Users">
               Users
-            </Link>
-            <Link className="nav-link" to="Admin">
+            </NavLink>
+            <NavLink className="nav-link" to="Admin">
               Admin
-            </Link>
+            </NavLink>
             {/* <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#link">Users</Nav.Link>
             <Nav.Link href="#link">Admin</Nav.Link> */}
