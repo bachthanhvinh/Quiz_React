60d5b413b593b570ffb4ba599e172f921ea87f39
Vinhz <bachthanhvinh12@gmail.com>
2025-04-19 21:20:27 +0700

    process Data - xử lý database phía front-end

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getDataQuestion } from "../../services/apiServices";
+import _ from "lodash";
 
 function DetaiQuiz() {
   const param = useParams();
@@ -11,7 +12,34 @@ function DetaiQuiz() {
 
   const fetchdataQuiz = async () => {
     const res = await getDataQuestion(quizId);
-    console.log(res);
+    let raw = res.DT;
+    console.log(raw);
+    if (res && res.EC === 0) {
+      let data = _.chain(raw)
+        // Group the elements of Array based on `color` property
+        .groupBy("id")
+        // `key` is group's name (color), `value` is the array of objects
+        .map((value, key) => {
+          let answersUser = [];
+          let description,
+            image = null;
+          value.map((item, index) => {
+            if (index === 0) {
+              description = item.description;
+              image = item.image;
+            }
+            answersUser.push(item.answers.description);
+          });
+          return {
+            questionId: +key,
+            answers: answersUser,
+            description,
+            image,
+          };
+        })
+        .value();
+      console.log(data);
+    }
   };
   return (
     <>
